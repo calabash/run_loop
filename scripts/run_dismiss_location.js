06c68d97563d1bad5c04c1b480de37e0fbc20d2e
Karl Krukow <karl.krukow@gmail.com>
2012-12-18 15:42:03 +0100

    support for eval

--- scripts/run_dismiss_location.js
+++ scripts/run_dismiss_location.js
@@ -56,7 +56,8 @@ UIATarget.onAlert = function (alert) {
 
 function performAction(action, data) {
     UIALogger.logMessage("perform action:" + action);
-    var actionTaken = true;
+    var actionTaken = true,
+        res = null;
     switch (action) {
         case "setLocation":
             target.setLocation({"latitude":data.latitude, "longitude":data.longitude});
@@ -71,10 +72,20 @@ function performAction(action, data) {
             screenshot_count += 1;
             target.captureScreenWithName(data.name || ("screenshot_" + screenshot_count));
             break;
+        case "eval":
+            try {
+                res = eval(data);
+            } catch (e) {
+                if (e) {
+                    UIALogger.logMessage(e.toString());
+                }
+            }
+            break;
     }
     if (actionTaken && !data.preserve) {
         target.frontMostApp().setPreferencesValueForKey(null, "__run_loop_action");
     }
+    return res;
 
 }
 
@@ -82,15 +93,26 @@ UIALogger.logStart("RunLoop");
 
 var app = target.frontMostApp(),
     val,
+    res,
     count = 0,
     action = null;
 while (true) {
-    target.delay(0.3);
+    target.delay(0.1);
     val = app.preferencesValueForKey("__run_loop_action");
     if (val && typeof val == 'object') {
         action = val.action;
         performAction(action, val);
     }
+    else if (val && typeof val == 'string') {
+        try {
+            res = performAction("eval", val);
+            if (res) {
+                UIALogger.logMessage(res.toString());
+            }
+        }
+
+    }
+
     count += 1;
 }
 
