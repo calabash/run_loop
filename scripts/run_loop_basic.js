dd374e1fc6b6a172042afeac827fa619154ea7ab
Joshua Moody <joshuajmoody@gmail.com>
2015-01-09 19:12:43 +0100

    Delete dismiss-system-level alert code from scripts

--- scripts/run_loop_basic.js
+++ scripts/run_loop_basic.js
@@ -148,61 +148,11 @@ if (typeof JSON !== 'object') {
     }
 }());
 
-function findAlertViewText(alert) {
-    if (!alert) {
-        return false;
-    }
-    var txt = alert.name(),
-        txts;
-    if (txt == null) {
-        txts = alert.staticTexts();
-        if (txts != null && txts.length > 0) {
-            txt = txts[0].name();
-        }
-    }
-    return txt;
-}
-
-function isExternallyGeneratedAlert(alert) {
-    var exps =
-                [
-                    // Location Alerts
-                    ["OK", /vil bruge din aktuelle placering/],
-                    ["OK", /Would Like to Use Your Current Location/],
-                    ["Allow", /access your location/],
-                    ["Ja", /Darf (?:.)+ Ihren aktuellen Ort verwenden/],
-                    ["OK", /Location Accuracy/],
-                    ["OK", /запрашивает разрешение на использование Ващей текущей пгеопозиции/],
-
-                    // Notifications
-                    ["OK", /Would Like to Send You Notifications/],
-
-                    // Photos
-                    ["OK", /Would Like to Access Your Photos/],
-
-                    // Contacts
-                    ["OK", /Would Like to Access Your Contacts/]
-                ],
-          ans, exp,
-          txt;
-
-    txt = findAlertViewText(alert);
-    Log.output({"output":"alert: "+txt}, true);
-    for (var i = 0; i < exps.length; i++) {
-        ans = exps[i][0];
-        exp = exps[i][1];
-        if (exp.test(txt)) {
-            return ans;
-        }
-    }
-    return false;
-}
-
 UIATarget.onAlert = function (alert) {
     Log.output({"output":"on alert"}, true);
     var target = UIATarget.localTarget();
     target.pushTimeout(10);
-    function attemptTouchOKOnLocation(retry_count) {
+    function attemptTouchDefaultAlertButton(retry_count) {
         retry_count = retry_count || 0;
         if (retry_count >= 5) {
             Log.output("Maxed out retry (5) - unable to dismiss location dialog.");
@@ -220,11 +170,11 @@ UIATarget.onAlert = function (alert) {
                 Log.output(e.toString());
             }
             target.delay(1);
-            attemptTouchOKOnLocation(retry_count + 1);
+            attemptTouchDefaultAlertButton(retry_count + 1);
         }
     }
 
-    attemptTouchOKOnLocation(0);
+    attemptTouchDefaultAlertButton(0);
     target.popTimeout();
     return true;
 };
--- scripts/run_loop_host.js
+++ scripts/run_loop_host.js
@@ -159,8 +159,6 @@ var timeoutScriptPath = "$TIMEOUT_SCRIPT_PATH",
 
 
 
-
-
 var _expectedIndex = 0,//expected index of next command
     _actualIndex,//actual index of next command by reading commandPath
     _index,//index of ':' char in command
@@ -168,58 +166,6 @@ var _expectedIndex = 0,//expected index of next command
     _result,//result of eval
     _input,//command
     _process;//host command process
-
-
-
-function findAlertViewText(alert) {
-    if (!alert) {
-        return false;
-    }
-    var txt = alert.name(),
-        txts;
-    if (txt == null) {
-        txts = alert.staticTexts();
-        if (txts != null && txts.length > 0) {
-            txt = txts[0].name();
-        }
-    }
-    return txt;
-}
-
-function isExternallyGeneratedAlert(alert) {
-    var exps =
-                [
-                    // Location Alerts
-                    ["OK", /vil bruge din aktuelle placering/],
-                    ["OK", /Would Like to Use Your Current Location/],
-                    ["Allow", /access your location/],
-                    ["Ja", /Darf (?:.)+ Ihren aktuellen Ort verwenden/],
-                    ["OK", /Location Accuracy/],
-                    ["OK", /запрашивает разрешение на использование Ващей текущей пгеопозиции/],
-
-                    // Notifications
-                    ["OK", /Would Like to Send You Notifications/],
-
-                    // Photos
-                    ["OK", /Would Like to Access Your Photos/],
-
-                    // Contacts
-                    ["OK", /Would Like to Access Your Contacts/]
-        ],
-        ans, exp,
-        txt;
-
-    txt = findAlertViewText(alert);
-    Log.output({"output":"alert: "+txt}, true);
-    for (var i = 0; i < exps.length; i++) {
-        ans = exps[i][0];
-        exp = exps[i][1];
-        if (exp.test(txt)) {
-            return ans;
-        }
-    }
-    return false;
-}
 
 UIATarget.onAlert = function (alert) {
     Log.output({"output":"on alert"}, true);
