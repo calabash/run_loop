e6229ef0940320dc58b4c21e2c16f708068e9928
Joshua Moody <joshuajmoody@gmail.com>
2015-02-25 17:09:38 +0100

    Refactor the inject_dylib example

--- spec/integration/dylib_injector_spec.rb
+++ spec/integration/dylib_injector_spec.rb
@@ -13,18 +13,19 @@ describe RunLoop::DylibInjector do
     RunLoop::SimControl.new.reset_sim_content_and_settings
   }
 
-  describe 'injecting a dylib targeting the simulator with' do
-    it "Xcode #{Resources.shared.current_xcode_version}" do
-      ENV['DEBUG'] = '1'
-      device = select_random_shutdown_sim
-      abp = Resources.shared.app_bundle_path
-      bridge = RunLoop::Simctl::Bridge.new(device, abp)
-      expect(bridge.launch).to be == true
+  describe '#inject_dylib' do
+    it 'targeting the simulator' do
+      Resources.shared.with_debugging do
+        device = select_random_shutdown_sim
+        abp = Resources.shared.app_bundle_path
+        bridge = RunLoop::Simctl::Bridge.new(device, abp)
+        expect(bridge.launch).to be == true
 
-      app = RunLoop::App.new(abp)
-      dylib = Resources.shared.sim_dylib_path
-      injector = RunLoop::DylibInjector.new(app.executable_name, dylib)
-      expect(injector.inject_dylib).to be == true
+        app = RunLoop::App.new(abp)
+        dylib = Resources.shared.sim_dylib_path
+        injector = RunLoop::DylibInjector.new(app.executable_name, dylib)
+        expect(injector.inject_dylib).to be == true
+      end
     end
   end
 end
