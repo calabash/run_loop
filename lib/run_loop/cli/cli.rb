ebc94d604e6ca577365ce7f9f69cb0c104f5656b
Joshua Moody <joshuajmoody@gmail.com>
2015-03-28 02:51:15 +0100

    Stub out the instruments commands

--- lib/run_loop/cli/cli.rb
+++ lib/run_loop/cli/cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'run_loop'
+require 'run_loop/cli/instruments'
 
 trap 'SIGINT' do
   puts 'Trapped SIGINT - exiting'
@@ -7,20 +8,26 @@ trap 'SIGINT' do
 end
 
 module RunLoop
-
   class ValidationError < Thor::InvocationError
   end
 
-  class CLI < Thor
-    include Thor::Actions
+  module CLI
 
-    def self.exit_on_failure?
-      true
-    end
+    class Tool < Thor
+      include Thor::Actions
 
-    desc 'version', 'Prints version of the run_loop gem'
-    def version
-      puts RunLoop::VERSION
+      def self.exit_on_failure?
+        true
+      end
+
+      desc 'version', 'Prints version of the run_loop gem'
+      def version
+        puts RunLoop::VERSION
+      end
+
+      desc 'instruments', "Interact with Xcode's command-line instruments"
+      subcommand 'instruments', RunLoop::CLI::Instruments
+
     end
   end
 end
---
+++ lib/run_loop/cli/instruments.rb
@@ -0,0 +1,111 @@
+require 'thor'
+require 'run_loop'
+
+module RunLoop
+  module CLI
+    class Instruments < Thor
+
+      attr_accessor :signal
+
+      desc 'instruments quit', 'Send a kill signal to all instruments processes.'
+
+      method_option 'signal',
+                    :desc => 'The kill signal to send.',
+                    :aliases => '-s',
+                    :required => false,
+                    :default => 'TERM',
+                    :type => :string
+
+      method_option 'debug',
+                    :desc => 'Enable debug logging.',
+                    :aliases => '-v',
+                    :required => false,
+                    :default => false,
+                    :type => :boolean
+
+
+      def quit
+
+      end
+
+
+      desc 'instruments launch [--app | [--ipa | --bundle-id]] [OPTIONS]', 'Launch an app with instruments.'
+
+# This is the description we want, but Thor doesn't handle newlines well(?).
+# long_desc <<EOF
+# # Launch App.app on a simulator.
+# $ be run-loop instruments launch --app /path/to/App.app
+#
+# # Launch App.ipa on a device; bundle id will be extracted.
+# $ be run-loop instruments launch --ipa /path/to/App.ipa
+#
+# # Launch the app with bundle id on a device.
+# $ be run-loop instruments launch --bundle-id com.example.MyApp-cal
+#
+# You can pass arguments to application as a comma separated list.
+# --args -NSShowNonLocalizedStrings,YES,-AppleLanguages,(de)
+# --args -com.apple.CoreData.SQLDebug,3'
+# EOF
+
+      method_option 'device',
+                    :desc => 'The device UDID or simulator identifier.',
+                    :aliases => '-d',
+                    :required => false,
+                    :type => :string
+
+      method_option 'app',
+                    :desc => 'Path to a .app bundle.',
+                    :aliases => '-a',
+                    :required => false,
+                    :type => :string
+
+      method_option 'ipa',
+                    :desc => 'Path to an .ipa bundle.',
+                    :aliases => '-i',
+                    :required => false,
+                    :type => :string
+
+      method_option 'bundle-id',
+                    :desc => 'Path to an .ipa bundle.',
+                    :aliases => '-b',
+                    :required => false,
+                    :type => :string
+
+      method_option 'template',
+                    :desc => 'Path to an automation template.',
+                    :aliases => '-t',
+                    :required => false,
+                    :type => :string
+
+      method_option 'args',
+                    :desc => 'Arguments to pass to the app.',
+                    :required => false,
+                    :type => :string
+
+      method_option 'debug',
+                    :desc => 'Enable debug logging.',
+                    :aliases => '-v',
+                    :required => false,
+                    :default => false,
+                    :type => :boolean
+
+      def launch
+
+      end
+
+      no_commands{
+        def validate_launch_args(options)
+
+        end
+
+        def validate_app_ipa_or_bundle_id(options)
+
+        end
+
+        def validate_args(options)
+
+        end
+      }
+    end
+  end
+end
