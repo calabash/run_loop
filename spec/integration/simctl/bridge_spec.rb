0456d3d86eaa7551abdf32c10046ae680e96aaad
Joshua Moody <joshuajmoody@gmail.com>
2015-04-11 17:51:34 +0200

    Refactor bridge integration specs

--- spec/integration/simctl/bridge_spec.rb
+++ spec/integration/simctl/bridge_spec.rb
@@ -1,36 +1,30 @@
 describe RunLoop::Simctl::Bridge do
 
-  def select_random_shutdown_sim
-    simctl = RunLoop::SimControl.new
-    simctl.simulators.shuffle.detect do |device|
+  let (:abp) { Resources.shared.app_bundle_path }
+  let (:sim_control) { RunLoop::SimControl.new }
+  let (:device) {
+    sim_control.simulators.shuffle.detect do |device|
       [device.state == 'Shutdown',
-       device.name != 'rspec-test-device',
+       device.name != 'rspec-0test-device',
        !device.name[/Resizable/,0]].all?
     end
-  end
+  }
+
+  let(:bridge) { RunLoop::Simctl::Bridge.new(device, abp) }
 
   it 'can launch a specific simulator' do
-    device = select_random_shutdown_sim
-    abp = Resources.shared.app_bundle_path
-    bridge = RunLoop::Simctl::Bridge.new(device, abp)
     bridge.launch_simulator
   end
 
   it 'can install an app on a simulator and launch it' do
-    device = select_random_shutdown_sim
-    abp = Resources.shared.app_bundle_path
-    bridge = RunLoop::Simctl::Bridge.new(device, abp)
     expect(bridge.launch).to be == true
   end
 
   it 'can install an app, launch it, and uninstall it' do
-    device = select_random_shutdown_sim
-    abp = Resources.shared.app_bundle_path
-    bridge = RunLoop::Simctl::Bridge.new(device, abp)
     expect(bridge.launch).to be == true
 
-    bridge = RunLoop::Simctl::Bridge.new(device, abp)
-    expect(bridge.uninstall).to be == true
+    new_bridge = RunLoop::Simctl::Bridge.new(device, abp)
+    expect(new_bridge.uninstall).to be == true
+    expect(new_bridge.app_is_installed?).to be_falsey
   end
-
 end
