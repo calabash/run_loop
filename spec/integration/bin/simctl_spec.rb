950296413c8ddd140dfaa20036609df895f091ad
Joshua Moody <joshuajmoody@gmail.com>
2015-04-28 14:39:08 +0200

    Integration examples for run-loop simctl install

--- spec/integration/bin/simctl_spec.rb
+++ spec/integration/bin/simctl_spec.rb
@@ -3,14 +3,15 @@ unless Luffa::Environment.travis_ci?
 
   describe RunLoop::CLI::Simctl do
 
+    let(:bridge) {
+      default = RunLoop::Core.default_simulator
+      device = RunLoop::SimControl.new.simulators.detect do |sim|
+        sim.instruments_identifier == default
+      end
+      RunLoop::Simctl::Bridge.new(device, Resources.shared.app_bundle_path)
+    }
+
     describe 'bundle exec run-loop simctl booted' do
-      let(:bridge) {
-        default = RunLoop::Core.default_simulator
-        device = RunLoop::SimControl.new.simulators.detect do |sim|
-          sim.instruments_identifier == default
-        end
-        RunLoop::Simctl::Bridge.new(device, Resources.shared.app_bundle_path)
-      }
 
       let(:cmd) { 'bundle exec run-loop simctl booted' }
 
@@ -36,6 +37,106 @@ unless Luffa::Environment.travis_ci?
           expect(out[/iPhone 5s/, 0]).to be_truthy
           expect(out[/x86_64/, 0]).to be_truthy
           expect(process_status.value.exitstatus).to be == 0
+        end
+      end
+    end
+
+    describe 'run-loop simctl install' do
+
+      let(:cmd) {
+        'run-loop simctl install --debug --app spec/resources/CalSmoke.app'.split(' ')
+      }
+
+      describe 'app is not installed' do
+
+        before {
+          bridge.uninstall
+        }
+
+        let(:device) { bridge.device }
+
+        it 'can install an app on default simulator' do
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
+        end
+
+        it 'can install an app on simulator using UDID' do
+          cmd << '--device'
+          cmd << device.udid
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
+        end
+
+        it 'can install an app on simulator using name' do
+          cmd << '--device'
+          cmd << device.instruments_identifier
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
+        end
+      end
+
+      describe 'app is installed, but not different' do
+
+        before {
+          bridge.install
+        }
+
+        it 'skips the install' do
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(out[/Will not re-install 'com.xamarin.CalSmoke' because the SHAs match/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
+        end
+      end
+
+      describe 'app is installed, but different' do
+        it 're-installs the app' do
+          app_bundle_dir = bridge.fetch_app_dir
+          path = FileUtils.touch(File.join(app_bundle_dir, 'tmp.txt')).first
+          File.open(path, 'w') do |file|
+            file.write('some text')
+          end
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(out[/Will re-install 'com.xamarin.CalSmoke' because the SHAs don't match/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
+        end
+      end
+
+      describe '--force' do
+        it 'forces a re-install of the app' do
+          cmd << '--force'
+          Open3.popen3(cmd.shift, *cmd) do |_, stdout, stderr, process_status|
+            out = stdout.read.strip
+            expect(out[/iPhone 5s/, 0]).to be_truthy
+            expect(out[/Installed 'com.xamarin.CalSmoke'/, 0]).to be_truthy
+            expect(out[/Will force a re-install/, 0]).to be_truthy
+            expect(stderr.read).to be == ''
+            expect(process_status.value.exitstatus).to be == 0
+          end
         end
       end
     end
