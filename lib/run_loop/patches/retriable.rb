7d510cd66b23e6eee7f47b3270900420ed22b2fc
Joshua Moody <joshuajmoody@gmail.com>
2015-04-29 04:50:51 +0200

    Patch for retriable 1.4.0

--- lib/run_loop/patches/retriable.rb
+++ lib/run_loop/patches/retriable.rb
@@ -23,3 +23,23 @@ module RunLoop
     end
   end
 end
+
+# Only in retriable 1.4.0
+unless Retriable.public_instance_methods.include?(:retriable)
+  require 'retriable/retry'
+  module Retriable
+    extend self
+
+    def retriable(opts = {}, &block)
+      raise LocalJumpError unless block_given?
+
+      Retry.new do |r|
+        r.tries    = opts[:tries] if opts[:tries]
+        r.on       = opts[:on] if opts[:on]
+        r.interval = opts[:interval] if opts[:interval]
+        r.timeout  = opts[:timeout] if opts[:timeout]
+        r.on_retry = opts[:on_retry] if opts[:on_retry]
+      end.perform(&block)
+    end
+  end
+end
