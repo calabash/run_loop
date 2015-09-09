1663e5b0d9f4513aee986701466f5329a4725f7f
Joshua Moody <joshuajmoody@gmail.com>
2015-09-09 12:44:42 +0200

    LifeCycle::CoreSimulator finding files and directories

---
+++ lib/run_loop/life_cycle/core_simulator.rb
@@ -0,0 +1,200 @@
+module RunLoop
+  module LifeCycle
+
+    class CoreSimulator
+
+      # @!visibility private
+      METADATA_PLIST = '.com.apple.mobile_container_manager.metadata.plist'
+
+      # @!visibility private
+      CORE_SIMULATOR_DEVICE_DIR = File.expand_path('~/Library/Developer/CoreSimulator/Devices')
+
+      # @!visibility private
+      # Pattern.
+      # [ '< process name >', < send term first > ]
+      MANAGED_PROCESSES =
+            [
+                  # This process is a daemon, and requires 'KILL' to terminate.
+                  # Killing the process is fast, but it takes a long time to
+                  # restart.
+                  # ['com.apple.CoreSimulator.CoreSimulatorService', false],
+
+                  # Probably do not need to quit this, but it is tempting to do so.
+                  #['com.apple.CoreSimulator.SimVerificationService', false],
+
+                  # Started by Xamarin Studio, this is the parent process of the
+                  # processes launched by Xamarin's interaction with
+                  # CoreSimulatorBridge.
+                  ['csproxy', true],
+
+                  # Yes.
+                  ['SimulatorBridge', true],
+                  ['configd_sim', true],
+                  ['launchd_sim', true],
+
+                  # Does not always appear.
+                  ['CoreSimulatorBridge', true],
+
+                  # Xcode 7
+                  ['ids_simd', true]
+            ]
+
+      attr_reader :app
+      attr_reader :device
+      attr_reader :pbuddy
+
+      # @param [RunLoop::App] app The application.
+      # @param [RunLoop::Device] device The device.
+      def initialize(app, device)
+        @app = app
+        @device = device
+
+        # In order to manage the app on the device, we need to manage the
+        # CoreSimulator processes.
+        terminate_core_simulator_processes
+      end
+
+      # @!visibility private
+      def pbuddy
+        @pbuddy ||= RunLoop::PlistBuddy.new
+      end
+
+      # @!visibility private
+      def sdk_gte_8?
+        device.version >= RunLoop::Version.new('8.0')
+      end
+
+      # The data directory for the the device.
+      #
+      # ~/Library/Developer/CoreSimulator/Devices/<UDID>/data
+      def device_data_dir
+        @device_data_dir ||= File.join(CORE_SIMULATOR_DEVICE_DIR, device.udid, 'data')
+      end
+
+      # The applications directory for the device.
+      #
+      # ~/Library/Developer/CoreSimulator/Devices/<UDID>/Containers/Bundle/Application
+      def device_applications_dir
+        @device_app_dir ||= lambda do
+          if sdk_gte_8?
+            File.join(device_data_dir, 'Containers', 'Bundle', 'Application')
+          else
+            File.join(device_data_dir, 'Applications')
+          end
+        end.call
+      end
+
+      # The sandbox directory for the app.
+      #
+      # ~/Library/Developer/CoreSimulator/Devices/<UDID>/Containers/Data/Application
+      #
+      # Contains Library, Documents, and tmp directories.
+      def app_sandbox_dir
+        app_install_dir = installed_app_bundle_dir
+        return nil if app_install_dir.nil?
+        if sdk_gte_8?
+          app_sandbox_dir_sdk_gte_8
+        else
+          app_install_dir
+        end
+      end
+
+      # The Library directory in the sandbox.
+      def app_library_dir
+        base_dir = app_sandbox_dir
+        if base_dir.nil?
+          nil
+        else
+          File.join(base_dir, 'Library')
+        end
+      end
+
+      # The Library/Preferences directory in the sandbox.
+      def app_library_preferences_dir
+        base_dir = app_library_dir
+        if base_dir.nil?
+          nil
+        else
+          File.join(base_dir, 'Preferences')
+        end
+      end
+
+      # The Documents directory in the sandbox.
+      def app_documents_dir
+        base_dir = app_sandbox_dir
+        if base_dir.nil?
+          nil
+        else
+          File.join(base_dir, 'Documents')
+        end
+      end
+
+      # The tmp directory in the sandbox.
+      def app_tmp_dir
+        base_dir = app_sandbox_dir
+        if base_dir.nil?
+          nil
+        else
+          File.join(base_dir, 'tmp')
+        end
+      end
+
+      # Is this app installed?
+      def app_is_installed?
+        !installed_app_bundle_dir.nil?
+      end
+
+      # @!visibility private
+      #
+      # Returns the path to the installed app bundle directory (.app).
+      #
+      # If this method returns nil, the app is not installed.
+      def installed_app_bundle_dir
+        sim_app_dir = device_applications_dir
+        return nil if !File.exist?(sim_app_dir)
+        Dir.glob("#{sim_app_dir}/**/*.app").find do |path|
+          RunLoop::App.new(path).bundle_identifier == app.bundle_identifier
+        end
+      end
+
+      private
+
+      # @!visibility private
+      def app_sandbox_dir_sdk_gte_8
+        containers_data_dir = File.join(device_data_dir, 'Containers', 'Data', 'Application')
+        apps = Dir.glob("#{containers_data_dir}/**/#{METADATA_PLIST}")
+        match = apps.find do |metadata_plist|
+          pbuddy.plist_read('MCMMetadataIdentifier', metadata_plist) == app.bundle_identifier
+        end
+        if match
+          File.dirname(match)
+        else
+          nil
+        end
+      end
+
+      # @!visibility private
+      def terminate_core_simulator_processes
+        MANAGED_PROCESSES.each do |pair|
+          name = pair[0]
+          send_term = pair[1]
+          pids = RunLoop::ProcessWaiter.new(name).pids
+          pids.each do |pid|
+
+            if send_term
+              term = RunLoop::ProcessTerminator.new(pid, 'TERM', name)
+              killed = term.kill_process
+            else
+              killed = false
+            end
+
+            unless killed
+              term = RunLoop::ProcessTerminator.new(pid, 'KILL', name)
+              term.kill_process
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/life_cycle/core_simulator_spec.rb
@@ -0,0 +1,273 @@
+describe RunLoop::LifeCycle::CoreSimulator do
+
+  before do
+    allow_any_instance_of(RunLoop::LifeCycle::CoreSimulator).to(
+          receive(:terminate_core_simulator_processes).and_return true
+    )
+  end
+
+  it '.new' do
+    core_sim = RunLoop::LifeCycle::CoreSimulator.new(:a, :b)
+
+    expect(core_sim.instance_variable_get(:@app)).to be == :a
+    expect(core_sim.instance_variable_get(:@device)).to be == :b
+  end
+
+  it '#pbuddy' do
+    core_sim = RunLoop::LifeCycle::CoreSimulator.new(:a, :b)
+
+    pbuddy = core_sim.pbuddy
+    expect(pbuddy).to be_a_kind_of(RunLoop::PlistBuddy)
+    expect(core_sim.pbuddy).to be == pbuddy
+    expect(core_sim.instance_variable_get(:@pbuddy)).to be == pbuddy
+  end
+
+  let(:app) { RunLoop::App.new(Resources.shared.cal_app_bundle_path) }
+
+  describe 'Mocked file system' do
+    let(:device) { RunLoop::Device.new('iPhone 5s', '8.1',
+                                       'A08334BE-77BD-4A2F-BA25-A0E8251A1A80') }
+    let(:core_sim) { RunLoop::LifeCycle::CoreSimulator.new(app, device) }
+
+    describe '#sdk_gte_8?' do
+      it 'returns true' do
+        expect(core_sim.sdk_gte_8?).to be_truthy
+      end
+
+      it 'returns false' do
+        expect(device).to receive(:version).and_return RunLoop::Version.new('7.1')
+
+        expect(core_sim.sdk_gte_8?).to be_falsey
+      end
+    end
+
+    it '#device_data_dir' do
+      base = RunLoop::LifeCycle::CoreSimulator::CORE_SIMULATOR_DEVICE_DIR
+      expected = File.join(base, device.udid, 'data')
+
+      actual = core_sim.device_data_dir
+      expect(actual).to be == expected
+      expect(core_sim.instance_variable_get(:@device_data_dir)).to be == expected
+    end
+
+    describe '#device_applications_dir' do
+      before do
+        expect(core_sim).to receive(:device_data_dir).and_return '/'
+      end
+
+      it 'iOS >= 8.0' do
+        expected = '/Containers/Bundle/Application'
+
+        expect(core_sim.device_applications_dir).to be == expected
+        expect(core_sim.instance_variable_get(:@device_app_dir)).to be == expected
+      end
+
+      it 'iOS < 8.0' do
+        expect(device).to receive(:version).and_return RunLoop::Version.new('7.1')
+
+        expected = '/Applications'
+
+        expect(core_sim.device_applications_dir).to be == expected
+        expect(core_sim.instance_variable_get(:@device_app_dir)).to be == expected
+      end
+    end
+
+    describe '#app_sandbox_dir' do
+      it 'returns nil if there is no app bundle' do
+        expect(core_sim).to receive(:installed_app_bundle_dir).and_return nil
+
+        expect(core_sim.app_sandbox_dir).to be == nil
+      end
+
+      describe 'app is installed' do
+        before do
+          expect(core_sim).to receive(:installed_app_bundle_dir).and_return '/'
+        end
+
+        it 'iOS >= 8.0' do
+          expect(core_sim).to receive(:app_sandbox_dir_sdk_gte_8).and_return 'match'
+
+          expect(core_sim.app_sandbox_dir).to be == 'match'
+        end
+
+        it 'iOS < 8.0' do
+          expect(device).to receive(:version).and_return RunLoop::Version.new('7.1')
+
+          expect(core_sim.app_sandbox_dir).to be == '/'
+        end
+      end
+    end
+
+    describe '#app_library_dir' do
+      it 'returns nil when app is not installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return nil
+
+        expect(core_sim.app_library_dir).to be == nil
+      end
+
+      it 'returns Library dir when app is installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return '/'
+
+        expect(core_sim.app_library_dir).to be == '/Library'
+      end
+    end
+
+    describe '#app_library_preferences_dir' do
+      it 'returns nil when app is not installed' do
+        expect(core_sim).to receive(:app_library_dir).and_return nil
+
+        expect(core_sim.app_library_preferences_dir).to be == nil
+      end
+
+      it 'returns Library/Preferences dir when app is installed' do
+        expect(core_sim).to receive(:app_library_dir).and_return '/'
+
+        expect(core_sim.app_library_preferences_dir).to be == '/Preferences'
+      end
+    end
+
+    describe '#app_documents_dir' do
+      it 'returns nil when app is not installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return nil
+
+        expect(core_sim.app_documents_dir).to be == nil
+      end
+
+      it 'returns Documents dir when app is installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return '/'
+
+        expect(core_sim.app_documents_dir).to be == '/Documents'
+      end
+    end
+
+    describe '#app_tmp_dir' do
+      it 'returns nil when app is not installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return nil
+
+        expect(core_sim.app_tmp_dir).to be == nil
+      end
+
+      it 'returns tmp dir when app is installed' do
+        expect(core_sim).to receive(:app_sandbox_dir).and_return '/'
+
+        expect(core_sim.app_tmp_dir).to be == '/tmp'
+      end
+    end
+
+    describe '#app_is_installed?' do
+      it 'returns false when app is not installed' do
+        expect(core_sim).to receive(:installed_app_bundle_dir).and_return nil
+
+        expect(core_sim.app_is_installed?).to be == false
+      end
+
+      it 'returns true when app is installed' do
+        expect(core_sim).to receive(:installed_app_bundle_dir).and_return '/'
+
+        expect(core_sim.app_is_installed?).to be == true
+      end
+    end
+  end
+
+  describe 'Canned file system' do
+
+    let(:tmp_dir) { Dir.mktmpdir }
+    let(:directory) { File.join(tmp_dir, 'CoreSimulator') }
+
+    let(:device_with_app) do
+      RunLoop::Device.new('iPhone 5s', '9.0', '69BD76CA-415A-4981-81AC-2CC9EE4FC177')
+    end
+
+    let(:device_without_app) do
+      RunLoop::Device.new('iPhone 5s', '8.4', '6386C48A-E029-4C1A-932D-355F652F66B9')
+    end
+
+    let(:sdk_71_device_with_app) do
+      RunLoop::Device.new('iPhone 5s', '7.1', 'B88A172B-CF92-4D3A-8A88-96FF4A6303D3')
+    end
+
+    let(:sdk_71_device_without_app) do
+      RunLoop::Device.new('iPhone 5', '7.1', '8DE4DF9B-09A4-4CFF-88E1-C62C88DD1503')
+    end
+
+    before do
+      source = File.join(Resources.shared.resources_dir, 'CoreSimulator')
+      FileUtils.cp_r(source, tmp_dir)
+      stub_const('RunLoop::LifeCycle::CoreSimulator::CORE_SIMULATOR_DEVICE_DIR',
+                 directory)
+    end
+
+    describe '#installed_app_bundle_dir' do
+      describe 'iOS >= 8' do
+        it 'app is installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, device_with_app)
+
+          actual = core_sim.installed_app_bundle_dir
+          expect(actual).to be_truthy
+          expect(File.exist?(actual)).to be_truthy
+        end
+
+        it 'app is not installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, device_without_app)
+
+          actual = core_sim.installed_app_bundle_dir
+          expect(actual).to be_falsey
+        end
+      end
+
+      describe 'iOS < 8' do
+        it 'app is installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, sdk_71_device_with_app)
+
+          actual = core_sim.installed_app_bundle_dir
+          expect(actual).to be_truthy
+          expect(File.exist?(actual)).to be_truthy
+        end
+
+        it 'app is not installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, sdk_71_device_without_app)
+
+          actual = core_sim.installed_app_bundle_dir
+          expect(actual).to be_falsey
+        end
+      end
+    end
+
+    describe '#app_sandbox_dir' do
+
+      describe 'iOS >= 8' do
+        it 'app is installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, device_with_app)
+
+          actual = core_sim.app_sandbox_dir
+          expect(actual).to be_truthy
+          expect(File.exist?(actual)).to be_truthy
+        end
+
+        it 'app is not installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, device_without_app)
+
+          actual = core_sim.app_sandbox_dir
+          expect(actual).to be_falsey
+        end
+      end
+
+      describe 'iOS < 8' do
+        it 'app is installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, sdk_71_device_with_app)
+
+          actual = core_sim.app_sandbox_dir
+          expect(actual).to be_truthy
+          expect(File.exist?(actual)).to be_truthy
+        end
+
+        it 'app is not installed' do
+          core_sim = RunLoop::LifeCycle::CoreSimulator.new(app, sdk_71_device_without_app)
+
+          actual = core_sim.app_sandbox_dir
+          expect(actual).to be_falsey
+        end
+      end
+    end
+  end
+end
