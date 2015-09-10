3599596cda8c15abadfdca2322b657f1cf701949
Joshua Moody <joshuajmoody@gmail.com>
2015-09-10 12:27:19 +0200

    Environment.with_debugging

--- lib/run_loop/environment.rb
+++ lib/run_loop/environment.rb
@@ -95,5 +95,15 @@ module RunLoop
         float
       end
     end
+
+    def self.with_debugging(&block)
+      original_value = ENV['DEBUG']
+      ENV['DEBUG'] = '1'
+      begin
+        block.call
+      ensure
+        ENV['DEBUG'] = original_value
+      end
+    end
   end
 end
