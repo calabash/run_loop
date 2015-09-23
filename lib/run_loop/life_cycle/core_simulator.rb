d9c7e062645659b63577f660390b385bd516992c
Joshua Moody <joshuajmoody@gmail.com>
2015-09-23 18:07:13 +0200

    CoreSimulator is a subclass of Simulator

--- lib/run_loop/life_cycle/core_simulator.rb
+++ lib/run_loop/life_cycle/core_simulator.rb
@@ -1,7 +1,7 @@
 module RunLoop
   module LifeCycle
 
-    class CoreSimulator
+    class CoreSimulator < Simulator
 
       require 'securerandom'
 
@@ -10,40 +10,6 @@ module RunLoop
 
       # @!visibility private
       CORE_SIMULATOR_DEVICE_DIR = File.expand_path('~/Library/Developer/CoreSimulator/Devices')
-
-      # @!visibility private
-      # Pattern.
-      # [ '< process name >', < send term first > ]
-      MANAGED_PROCESSES =
-            [
-                  # This process is a daemon, and requires 'KILL' to terminate.
-                  # Killing the process is fast, but it takes a long time to
-                  # restart.
-                  # ['com.apple.CoreSimulator.CoreSimulatorService', false],
-
-                  # Probably do not need to quit this, but it is tempting to do so.
-                  #['com.apple.CoreSimulator.SimVerificationService', false],
-
-                  # Started by Xamarin Studio, this is the parent process of the
-                  # processes launched by Xamarin's interaction with
-                  # CoreSimulatorBridge.
-                  ['csproxy', true],
-
-                  # Yes.
-                  ['SimulatorBridge', true],
-                  ['configd_sim', true],
-                  ['launchd_sim', true],
-
-                  # Does not always appear.
-                  ['CoreSimulatorBridge', true],
-
-                  # assetsd instances clobber each other and are not properly
-                  # killed when quiting the simulator.
-                  ['assetsd', true],
-
-                  # Xcode 7
-                  ['ids_simd', true]
-            ]
 
       # @!visibility private
       # How long to wait after the simulator has launched.
@@ -410,29 +376,6 @@ module RunLoop
           File.dirname(match)
         else
           nil
-        end
-      end
-
-      # @!visibility private
-      def terminate_core_simulator_processes
-        MANAGED_PROCESSES.each do |pair|
-          name = pair[0]
-          send_term = pair[1]
-          pids = RunLoop::ProcessWaiter.new(name).pids
-          pids.each do |pid|
-
-            if send_term
-              term = RunLoop::ProcessTerminator.new(pid, 'TERM', name)
-              killed = term.kill_process
-            else
-              killed = false
-            end
-
-            unless killed
-              term = RunLoop::ProcessTerminator.new(pid, 'KILL', name)
-              term.kill_process
-            end
-          end
         end
       end
 
