3f4b1ce6edf479a3c9e94e443ac4b0c17bc23004
Joshua Moody <joshuajmoody@gmail.com>
2015-10-26 18:37:22 +0100

    Dylib: rewrite existing specs

--- spec/lib/dylib_injector_spec.rb
+++ spec/lib/dylib_injector_spec.rb
@@ -1,13 +1,23 @@
 describe RunLoop::DylibInjector do
 
-  describe '.new' do
-    it 'creates a new object with attrs set' do
-      executable_name = 'executable name'
-      dylib_path = '/some/path'
-      lldb = RunLoop::DylibInjector.new(executable_name, dylib_path)
-      expect(lldb).to be_a_kind_of(RunLoop::DylibInjector)
-      expect(lldb.process_name).to be == executable_name
-      expect(lldb.dylib_path).to be == dylib_path
-    end
+  let(:executable) { "app name" }
+  let(:dylib) { "/some/path" }
+  let(:lldb) { RunLoop::DylibInjector.new(executable, dylib) }
+
+  it '.new' do
+    expect(lldb.process_name).to be == executable
+    expect(lldb.instance_variable_get(:@process_name)).to be == executable
+
+    expect(lldb.dylib_path).to be == dylib
+    expect(lldb.instance_variable_get(:@dylib_path)).to be == dylib
+  end
+
+  it "#xcrun" do
+    xcrun = lldb.xcrun
+
+    expect(lldb.xcrun).to be == xcrun
+    expect(lldb.instance_variable_get(:@xcrun)).to be == xcrun
+  end
+
   end
 end
