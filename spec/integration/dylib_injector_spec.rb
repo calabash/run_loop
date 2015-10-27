d32f6d70d98b6b9629fe91e94e8ab58911c7d039
Joshua Moody <joshuajmoody@gmail.com>
2015-10-27 12:05:02 +0100

    Dylib: integration spec is working

--- spec/integration/dylib_injector_spec.rb
+++ spec/integration/dylib_injector_spec.rb
@@ -5,35 +5,21 @@ if Resources.shared.core_simulator_env?
     let(:device) { Resources.shared.random_simulator_device }
     let(:app_bundle) { Resources.shared.app_bundle_path }
     let(:app) { RunLoop::App.new(app_bundle) }
-    let(:core_sim) { RunLoop::LifeCycle::CoreSimulator.new(app, device) }
+    let(:core_sim) { RunLoop::CoreSimulator.new(device, app) }
     let(:dylib) { Resources.shared.sim_dylib_path }
     let(:injector) { RunLoop::DylibInjector.new(app.executable_name, dylib) }
 
     before do
       stub_env({'DEBUG' => '1'})
-    end
-
-    describe '#inject_dylib_with_timeout' do
-      it 'targeting the simulator' do
-        core_sim.send(:launch)
-
-        expect do
-          injector.inject_dylib_with_timeout(1)
-        end.to raise_error RuntimeError
-      end
+      RunLoop::CoreSimulator.quit_simulator
     end
 
     describe '#retriable_inject_dylib' do
       it 'targeting the simulator' do
         core_sim.send(:launch)
-
-        vals = [false, false]
-        options = { retries: vals.count + 1}
-        expect(injector).to receive(:inject_dylib_with_timeout).exactly(vals.count).times.and_return(*vals)
-        expect(injector).to receive(:inject_dylib_with_timeout).and_call_original
-
-        expect(injector.retriable_inject_dylib(options)).to be == true
+        expect(injector.retriable_inject_dylib).to be == true
       end
     end
   end
 end
+
