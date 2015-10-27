22d9762975651c85621ff93750115e38ecf06cba
Joshua Moody <joshuajmoody@gmail.com>
2015-10-27 17:02:40 +0100

    Jenkins: run dylib integration spec

--- scripts/ci/jenkins/rspec-unit.sh
+++ scripts/ci/jenkins/rspec-unit.sh
@@ -4,5 +4,9 @@ rbenv local 2.2.3
 gem uninstall -Vax --force --no-abort-on-dependent run_loop
 bundle update
 rm -rf spec/reports
-rbenv exec bundle exec rspec spec/lib
+rbenv exec \
+  bundle exec \
+  rspec \
+  spec/lib \
+  spec/integration/dylib_injector_spec.rb
 
