56cf9575ce4ec4f6dbc5930c49a31edc299f3da2
Joshua Moody <joshuajmoody@gmail.com>
2016-01-06 11:00:33 +0100

    Spec: reproduce 'cannot find run_loop*js file'
    Unable to find file: run_loop_fast_uia.js #361

--- spec/integration/run_loop_spec.rb
+++ spec/integration/run_loop_spec.rb
@@ -8,5 +8,27 @@ describe 'RunLoop' do
       Resources.shared.launch_instruments_app
       expect { RunLoop.run }.to raise_error RuntimeError
     end
+
+    it "can be retried with raising an error" do
+      options = {
+        :app => Resources.shared.cal_app_bundle_path,
+        :uia_strategy => :preferences
+      }
+
+      run_options = {
+        :app => Resources.shared.cal_app_bundle_path,
+        :uia_strategy => :preferences,
+        :script => "run_loop_fast_uia.js"
+      }
+      expect(RunLoop::Core).to receive(:run_with_options).with(run_options).and_raise(ArgumentError)
+      expect(RunLoop::Core).to receive(:run_with_options).with(run_options).and_call_original
+
+      begin
+         RunLoop.run(options)
+      rescue ArgumentError => _
+        actual = RunLoop.run(options)
+        expect(actual[:uia_strategy]).to be == options[:uia_strategy]
+      end
+    end
   end
 end
