1d4a794887d5df9f1275ae32da678cd51f599a50
Joshua Moody <joshuajmoody@gmail.com>
2016-01-20 08:58:42 +0100

    Jenkins: combine scripts into run.sh

--- scripts/ci/jenkins/run.sh
+++ scripts/ci/jenkins/run.sh
@@ -1,7 +1,43 @@
 #!/usr/bin/env bash
 
-bundle update
-bundle exec run-loop simctl manage-processes
-scripts/ci/jenkins/rspec-unit.sh
-scripts/ci/jenkins/cli.sh
+if [ -n "${JENKINS_HOME}" ]; then
+  rbenv local 2.3.0
+  gem uninstall -Vax --force --no-abort-on-dependent run_loop
+fi
+
+rm -rf spec/reports
+
+if [ -n "${JENKINS_HOME}" ]; then
+  RBENV_EXEC="rbenv exec"
+else
+  RBENV_EXEC=
+fi
+
+$RBENV_EXEC bundle update
+
+# Unit tests
+$RBENV_EXEC bundle exec run-loop simctl manage-processes
+$RBENV_EXEC bundle exec rspec spec/lib
+
+# Integration tests
+$RBENV_EXEC bundle exec run-loop simctl manage-processes
+$RBENV_EXEC bundle exec rspec \
+  spec/integration/core_simulator_spec.rb \
+  spec/integration/xcode_spec.rb
+
+# CLI tests
+
+# Fail if any command exits non-zero
+set -e
+
+function execute {
+  echo "$(tput setaf 6)EXEC: $1 $(tput sgr0)"
+  $1
+}
+
+execute "$RBENV_EXEC bundle exec run-loop version"
+execute "$RBENV_EXEC bundle exec run-loop help"
+execute "$RBENV_EXEC bundle exec run-loop instruments help"
+execute "$RBENV_EXEC bundle exec run-loop simctl help"
+
 
