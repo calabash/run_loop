9b430112d77a89b5e7d057b0af99043d219cb4f3
Joshua Moody <joshuajmoody@gmail.com>
2016-01-21 10:03:58 +0100

    Template: doc methods and class as private

--- lib/run_loop/template.rb
+++ lib/run_loop/template.rb
@@ -2,21 +2,25 @@ require 'erb'
 
 module RunLoop
 
-  # class to break up javascript templates in to reusable chunks
+  # @!visibility private
+  # Class to break up javascript templates in to reusable chunks
   class UIAScriptTemplate < ERB
+
+    # @!visibility private
     def initialize(template_root, template_relative_path)
       @template_root = template_root
       @template = File.read(File.join(@template_root, template_relative_path))
       super(@template)
     end
 
+    # @!visibility private
     def render_template(template_relative_path)
       return UIAScriptTemplate.new(@template_root, template_relative_path).result
     end
 
+    # @!visibility private
     def result
       super(binding)
     end
   end
-
 end
