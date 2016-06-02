98bef283ce59e355d798275eebd27203cec039d3
Joshua Moody <joshuajmoody@gmail.com>
2016-06-02 20:53:35 +0200

    Xcodebuild: implements abstract launcher class

---
+++ lib/run_loop/device_agent/xcodebuild.rb
@@ -0,0 +1,96 @@
+
+module RunLoop
+  class Xcodebuild < RunLoop::Launcher
+
+    require "run_loop/shell"
+    include RunLoop::Shell
+
+    # @!visibility private
+    def self.log_file
+      path = File.join(Xcodebuild.dot_dir, "xcodebuild.log")
+      FileUtils.touch(path) if !File.exist?(path)
+      path
+    end
+
+    # @!visibility private
+    def to_s
+      "#<Xcodebuild #{workspace}>"
+    end
+
+    # @!visibility private
+    def inspect
+      to_s
+    end
+
+    # @!visibility private
+    def launch
+      workspace
+
+      shutdown
+
+      options = {:log_cmd => true}
+      self.shell(["pkill testmanagerd"], options)
+      self.shell(["pkill xcodebuild"], options)
+
+      if device.simulator?
+        # quits the simulator
+        sim = CoreSimulator.new(device, "")
+        sim.launch_simulator
+      else
+        # anything special about physical devices?
+      end
+
+      start = Time.now
+      RunLoop.log_debug("Waiting for CBX-Runner to build...")
+      pid = xcodebuild
+      RunLoop.log_debug("Took #{Time.now - start} seconds to build and launch CBX-Runner")
+      pid
+    end
+
+    # @!visibility private
+    def workspace
+      @workspace ||= lambda do
+        path = RunLoop::Environment.send(:cbxws)
+        if path
+          path
+        else
+          raise "The CBXWS env var is undefined. Are you a maintainer?"
+        end
+      end.call
+    end
+
+    # @!visibility private
+    def xcodebuild
+      env = {
+        "COMMAND_LINE_BUILD" => "1"
+      }
+
+      args = [
+        "xcrun",
+        "xcodebuild",
+        "-scheme", "CBXAppStub",
+        "-workspace", workspace,
+        "-config", "Debug",
+        "-destination",
+        "id=#{device.udid}",
+        "clean",
+        "test"
+      ]
+
+      log_file = Xcodebuild.xcodebuild_log_file
+
+      options = {
+        :out => log_file,
+        :err => log_file
+      }
+
+      command = "#{env.map.each { |k, v| "#{k}=#{v}" }.join(" ")} #{args.join(" ")}"
+      RunLoop.log_unix_cmd("#{command} >& #{log_file}")
+
+      pid = Process.spawn(env, *args, options)
+      Process.detach(pid)
+      pid.to_i
+    end
+  end
+end
+
---
+++ spec/lib/device_agent/xcodebuild_spec.rb
@@ -0,0 +1,55 @@
+
+describe RunLoop::Xcodebuild do
+  let(:device) { Resources.shared.default_simulator }
+  let(:xcodebuild) { RunLoop::Xcodebuild.new(device) }
+
+  describe "#workspace" do
+
+    it "raises an error if CBXWS is not defined" do
+      expect(RunLoop::Environment).to receive(:cbxws).and_return(nil)
+
+      expect do
+        xcodebuild.workspace
+      end.to raise_error(RuntimeError,
+                         /The CBXWS env var is undefined. Are you a maintainer/)
+    end
+
+    it "returns the path to the CBXDriver.xcworkspace" do
+      path = "path/to/CBXDriver.xcworkspace"
+      expect(RunLoop::Environment).to receive(:cbxws).and_return(path)
+
+      expect(xcodebuild.workspace).to be == path
+    end
+  end
+
+  describe "file system" do
+    let(:dot_dir) { File.expand_path(File.join("tmp", ".run-loop-xcuitest")) }
+    let(:xcuitest_dir) { File.join(dot_dir, "xcuitest") }
+
+    before do
+      FileUtils.mkdir_p(dot_dir)
+      allow(RunLoop::DotDir).to receive(:directory).and_return(dot_dir)
+    end
+
+    describe ".log_file" do
+      before do
+        FileUtils.mkdir_p(xcuitest_dir)
+      end
+
+      let(:path) { File.join(xcuitest_dir, "xcodebuild.log") }
+
+      it "creates a file" do
+        FileUtils.rm_rf(path)
+
+        expect(RunLoop::Xcodebuild.log_file).to be == path
+        expect(File.exist?(path)).to be_truthy
+      end
+
+      it "returns existing file path" do
+        FileUtils.touch(path)
+        expect(RunLoop::Xcodebuild.log_file).to be == path
+      end
+    end
+  end
+end
+
