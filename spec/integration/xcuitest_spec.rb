42b232191540ec842d0108b523f1beb8e68d943a
Joshua Moody <joshuajmoody@gmail.com>
2016-06-03 14:38:57 +0200

    Spec: better xcuitest integration tests

--- spec/integration/xcuitest_spec.rb
+++ spec/integration/xcuitest_spec.rb
@@ -5,10 +5,23 @@ describe RunLoop::XCUITest do
     allow(RunLoop::Environment).to receive(:debug?).and_return(true)
   end
 
-  it "#launch" do
-    device = Resources.shared.default_simulator
-    bundle_identifier = "com.apple.Preferences"
-    xcuitest = RunLoop::XCUITest.new(bundle_identifier, device)
-    xcuitest.launch
+  describe "#launch" do
+    let(:device) { Resources.shared.default_simulator }
+    let(:bundle_identifier) { "com.apple.Preferences" }
+
+    it "xcodebuild" do
+      cbx_launcher = RunLoop::DeviceAgent::Xcodebuild.new(device)
+      xcuitest = RunLoop::XCUITest.new(bundle_identifier, device, cbx_launcher)
+
+      expect do
+        xcuitest.launch
+      end.to raise_error(RuntimeError, /The CBXWS env var is undefined. Are you a maintainer/)
+    end
+
+    it "xctestctl" do
+      cbx_launcher = RunLoop::DeviceAgent::XCTestctl.new(device)
+      xcuitest = RunLoop::XCUITest.new(bundle_identifier, device, cbx_launcher)
+      xcuitest.launch
+    end
   end
 end
