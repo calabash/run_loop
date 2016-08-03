0ec02e498ef2971d26c39fb9146c759f8cbbb214
Joshua Moody <joshuajmoody@gmail.com>
2016-08-03 17:46:03 +0200

    Simctl: combine simctl life cycle methods into one example

--- spec/integration/simctl_spec.rb
+++ spec/integration/simctl_spec.rb
@@ -4,6 +4,12 @@ describe RunLoop::Simctl do
   let(:simctl) { Resources.shared.simctl }
   let(:sim_control) { Resources.shared.sim_control }
   let(:device) { Resources.shared.simctl.simulators.sample }
+  let(:app) { RunLoop::App.new(Resources.shared.cal_app_bundle_path) }
+  let(:core_sim) { RunLoop::CoreSimulator.new(device, app) }
+
+  before do
+    allow(RunLoop::Environment).to receive(:debug?).and_return(true)
+  end
 
   it "SimControl and Simctl return same simulators" do
     from_sim_control = sim_control.simulators
@@ -22,11 +28,29 @@ describe RunLoop::Simctl do
     expect(simctl.wait_for_shutdown(device, 0.1, 0)).to be_truthy
   end
 
-  it "#erase" do
+  it "handles the app life cycle" do
     timeout = RunLoop::CoreSimulator::DEFAULT_OPTIONS[:wait_for_state_timeout]
     delay = RunLoop::CoreSimulator::WAIT_FOR_SIMULATOR_STATE_INTERVAL
     expect(simctl.erase(device, timeout, delay)).to be_truthy
-  end
 
-  it "#launch"
+    device.simulator_wait_for_stable_state
+
+    core_sim.launch_simulator
+
+    timeout = RunLoop::CoreSimulator::DEFAULT_OPTIONS[:install_app_timeout]
+    expect(simctl.install(device, app, timeout)).to be_truthy
+    options = { :timeout => 10, :raise_on_timeout => true }
+    device.simulator_wait_for_stable_state
+
+    timeout = RunLoop::CoreSimulator::DEFAULT_OPTIONS[:launch_app_timeout]
+    expect(simctl.launch(device, app, timeout)).to be_truthy
+    RunLoop::ProcessWaiter.new(app.executable_name, options).wait_for_any
+    device.simulator_wait_for_stable_state
+
+    timeout = RunLoop::CoreSimulator::DEFAULT_OPTIONS[:uninstall_app_timeout]
+    expect(simctl.uninstall(device, app, timeout)).to be_truthy
+    device.simulator_wait_for_stable_state
+
+    RunLoop::CoreSimulator.quit_simulator
+  end
 end
