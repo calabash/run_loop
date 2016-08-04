0c9a80a22eb0855e7c659da849523b301e0c57fa
Joshua Escribano <escribirajoshua@gmail.com>
2016-08-03 17:24:50 -0700

    Adds wait options for core simulator spec to pass

--- spec/integration/core_simulator_spec.rb
+++ spec/integration/core_simulator_spec.rb
@@ -73,7 +73,8 @@ describe RunLoop::CoreSimulator do
 
   describe "#launch" do
     before do
-      Resources.shared.simctl.erase(simulator)
+      opts = RunLoop::CoreSimulator::DEFAULT_OPTIONS
+      Resources.shared.simctl.erase(simulator, opts[:launch_app_timeout], opts[:wait_for_state_timeout])
     end
 
     it "launches the app" do
