a735d972b27dc79c70c2bb1c9942fc6511de6216
Joshua Moody <joshuajmoody@gmail.com>
2017-08-28 14:05:47 +0200

    Device: adjust required processes for Xcode 9

--- lib/run_loop/device.rb
+++ lib/run_loop/device.rb
@@ -614,7 +614,9 @@ failed with this output:
     def simulator_required_child_processes
       @simulator_required_child_processes ||= begin
         required = ["backboardd", "installd", "SimulatorBridge", "SpringBoard"]
-        if xcode.version_gte_8? && version.major > 8
+        if xcode.version_gte_90?
+          required << "filecoordinationd"
+        elsif xcode.version_gte_8? && version.major > 8
           required << "medialibraryd"
         end
 
