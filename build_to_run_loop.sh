dd3686b7909e59901a62c9d001e2fb1858c274f3
Joshua Moody <joshuajmoody@gmail.com>
2018-03-28 16:06:46 +0200

    Bin: remove unused build script

--- build_to_run_loop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed s/"throw Error"/"throw new Error"/ < /Users/krukow/code/calabash-script/build/calabash_script.js | sed s/"return Error"/"return new Error"/ > ~/code/run_loop/scripts/calabash_script_uia.js && rake install
