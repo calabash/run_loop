6844f3233329d375328a7072b52895937df7fd98
Tikhon Polyakov <polyakrecords@gmail.com>
2018-05-21 16:56:58 +0700

    CI: add Jenkinsfile; add scripts for blue ocean pipeline

---
+++ scripts/ci/jenkins/simctl.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Ensure the correct CoreSimulatorService is loaded.
+
+xcrun simctl help >/dev/null 2>&1
+xcrun simctl help >/dev/null 2>&1
+xcrun simctl help >/dev/null 2>&1
