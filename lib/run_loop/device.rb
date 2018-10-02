547d6fd315dd8dbc487b6ad02694c8f61ed4166e
Serghei Moret <serghei.moret@xing.com>
2018-10-02 09:06:07 +0200

    add method description

--- lib/run_loop/device.rb
+++ lib/run_loop/device.rb
@@ -536,18 +536,20 @@ Could not update the Simulator languages.
       running_apps
     end
 
+    # Check if all the properties needed for the soft keyboard to appear are set
+    # Approach to negate 'true' and 'false' was chosen in order to do not reboot sim too often
+    # as this will cover the cases when the properties are not set which means that keyboard will be shown anyways
     def simulator_software_keyboard_will_show?
       plist = simulator_preferences_plist_path
 
       hw_keyboard_disabled = pbuddy.plist_read('HardwareKeyboardLastSeen', plist) != 'true'
-
       minimization_disabled = pbuddy.plist_read('AutomaticMinimizationEnabled', plist) != 'true'
-
       soft_keyboard_enabled = pbuddy.plist_read('SoftwareKeyboardShownByTouch', plist) != 'false'
 
       hw_keyboard_disabled && minimization_disabled && soft_keyboard_enabled
     end
 
+    # Add properties needed for soft keyboard to show into preferences plist
     def simulator_ensure_software_keyboard_will_show
       plist = simulator_preferences_plist_path
       pbuddy.plist_set('HardwareKeyboardLastSeen', 'bool', 'NO', plist)
