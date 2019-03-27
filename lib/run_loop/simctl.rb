993c1d1ae6af440eb89f2e7376c580199fbcf33c
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2019-03-27 15:58:10 +0100

    Minimal changes to make run_loop run with Xcode 10.2

--- lib/run_loop/simctl.rb
+++ lib/run_loop/simctl.rb
@@ -590,8 +590,14 @@ while trying to list devices.
 
     # @!visibility private
     def device_key_to_version(key)
-      str = key.split(" ").last
-      RunLoop::Version.new(str)
+      version_string = if key.include?(" ")
+        key.split(" ").last
+      else
+        regex = /(?<version>\d+-\d\z)/
+        matches = regex.match key
+        matches[:version].tr("-", ".")
+      end
+      RunLoop::Version.new(version_string)
     end
 
     # @!visibility private
